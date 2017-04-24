d20c42d984f38fcbfb0e208340894ec3babc0100
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-04-24 14:23:52 -0700

    added mock tests for filtering function

--- project/tests/test_todos.py
+++ project/tests/test_todos.py
@@ -2,7 +2,7 @@
 
 import requests
 from unittest.mock import Mock, patch
-from project.services import get_todos
+from project.services import get_todos, get_uncompleted_todos
 
 
 def test_request():
@@ -77,7 +77,7 @@ def test_getting_todos_when_response_is_ok(mock_get):
     """Use a mock to test functionality of todos.
 
     whenever the return_value is added to a mock, that mock is modified to be
-    run as a function, and by default it returns another mock object. 
+    run as a function, and by default it returns another mock object.
     """
     todos = [{
         'userId': 1,
@@ -109,3 +109,49 @@ def test_getting_todos_when_response_is_not_ok(mock_get):
 
     # If the response contains an error, I should get no todos.
     assert response is None
+
+
+@patch('project.services.get_todos')
+def test_getting_uncompleted_todos_when_todos_is_not_none(mock_get_todos):
+    """Test ability to filter response."""
+    todo1 = {
+        'userId': 1,
+        'id': 1,
+        'title': 'Make the bed',
+        'completed': False
+    }
+    todo2 = {
+        'userId': 1,
+        'id': 2,
+        'title': 'Walk the dog',
+        'completed': True
+    }
+
+    # Configure mock to return a response with a JSON-serialized list of todos.
+    mock_get_todos.return_value = Mock()
+    mock_get_todos.return_value.json.return_value = [todo1, todo2]
+
+    # Call the service, which will get a list of todos filtered on completed.
+    uncompleted_todos = get_uncompleted_todos()
+
+    # Confirm that the mock was called.
+    assert mock_get_todos.called
+
+    # Confirm that the expected filtered list of todos was returned.
+    assert uncompleted_todos == [todo1]
+
+
+@patch('project.services.get_todos')
+def test_getting_uncompleted_todos_when_todos_is_none(mock_get_todos):
+    """Test getting uncomplete todos."""
+    # Configure mock to return None.
+    mock_get_todos.return_value = None
+
+    # Call the service, which will return an empty list.
+    uncompleted_todos = get_uncompleted_todos()
+
+    # Confirm that the mock was called.
+    assert mock_get_todos.called
+
+    # Confirm that an empty list was returned.
+    assert uncompleted_todos == []
