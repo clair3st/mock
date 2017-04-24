01be9e0b13fd52c57cca90a8ea2d751af1d25e03
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-04-23 20:22:13 -0700

    added first mock test

--- project/tests/test_todos.py
+++ project/tests/test_todos.py
@@ -1,6 +1,7 @@
 """Testing an api."""
 
 import requests
+from unittest.mock import patch
 from project.services import get_todos
 
 
@@ -13,4 +14,16 @@ def test_request():
 def test_request_response():
     """Call the service which will send a request to the server."""
     response = get_todos()
+    assert response is not None
+
+
+@patch('project.services.requests.get')  # target get requests
+def test_getting_todos(mock_get):
+    """Configure the mock to return a response with an OK status code."""
+    mock_get.return_value.ok = True
+
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    # If the request is sent successfully, expect a response to be returned.
     assert response is not None
