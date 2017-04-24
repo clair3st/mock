0da72792e5a286f1e207e9292fb0e405d145ca30
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-04-23 20:30:34 -0700

    another way to patch

--- project/tests/test_todos.py
+++ project/tests/test_todos.py
@@ -27,3 +27,21 @@ def test_getting_todos(mock_get):
 
     # If the request is sent successfully, expect a response to be returned.
     assert response is not None
+
+
+def test_getting_todos_again():
+    """Explicitly patch a function within a block of code.
+
+    using a context manager. The with statement patches a function used by any
+    code in the code block. When the code block ends, the original function is
+    restored.
+    """
+    with patch('project.services.requests.get') as mock_get:
+        # Configure the mock to return a response with an OK status code.
+        mock_get.return_value.ok = True
+
+        # Call the service, which will send a request to the server.
+        response = get_todos()
+
+    # If the request is sent successfully, expect a response to be returned.
+    assert response is not None
