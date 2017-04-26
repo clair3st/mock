a61cf7e98b8f5e402db182c19a01577ce823b5d2
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-04-25 19:48:21 -0700

    refactored to have class based testing

--- project/tests/test_todos.py
+++ project/tests/test_todos.py
@@ -155,3 +155,108 @@ def test_getting_uncompleted_todos_when_todos_is_none(mock_get_todos):
 
     # Confirm that an empty list was returned.
     assert uncompleted_todos == []
+
+
+class TestTodos(object):
+    """Test todos in class layout."""
+
+    @classmethod
+    def setup_class(cls):
+        """Setup for testing env."""
+        cls.mock_get_patcher = patch('project.services.requests.get')
+        cls.mock_get = cls.mock_get_patcher.start()
+
+    @classmethod
+    def teardown_class(cls):
+        """Tear down."""
+        cls.mock_get_patcher.stop()
+
+    def test_getting_todos_when_response_is_ok(self):
+        """Use a mock to test functionality of todos.
+
+        whenever the return_value is added to a mock, that mock is modified to
+        be run as a function, and by default it returns another mock object.
+        """
+        todos = [{
+            'userId': 1,
+            'id': 1,
+            'title': 'Make the bed',
+            'completed': False
+        }]
+
+        # Configure the mock to return a response with an OK status code.
+        self.mock_get.return_value = Mock(ok=True)
+        # the mock should have a `json()` method that returns list of todos.
+        self.mock_get.return_value.json.return_value = todos
+
+        # Call the service, which will send a request to the server.
+        response = get_todos()
+
+        # If the request is sent successfully, expect response to be returned.
+        assert response.json() == todos
+
+    def test_getting_todos_when_response_is_not_ok(self):
+        """Test negative."""
+        # Configure the mock to not return a response with an OK status code.
+        self.mock_get.return_value.ok = False
+
+        # Call the service, which will send a request to the server.
+        response = get_todos()
+
+        # If the response contains an error, I should get no todos.
+        assert response is None
+
+
+class TestUncompletedTodos(object):
+    """Test uncompleted todos."""
+
+    @classmethod
+    def setup_class(cls):
+        """Setup test env."""
+        cls.mock_get_todos_patcher = patch('project.services.get_todos')
+        cls.mock_get_todos = cls.mock_get_todos_patcher.start()
+
+    @classmethod
+    def teardown_class(cls):
+        """Teardown function."""
+        cls.mock_get_todos_patcher.stop()
+
+    def test_getting_uncompleted_todos_when_todos_is_not_none(self):
+        """Test ability to filter response."""
+        todo1 = {
+            'userId': 1,
+            'id': 1,
+            'title': 'Make the bed',
+            'completed': False
+        }
+        todo2 = {
+            'userId': 1,
+            'id': 2,
+            'title': 'Walk the dog',
+            'completed': True
+        }
+
+        self.mock_get_todos.return_value = Mock()
+        self.mock_get_todos.return_value.json.return_value = [todo1, todo2]
+
+        uncompleted_todos = get_uncompleted_todos()
+
+        # Confirm that the mock was called.
+        assert self.mock_get_todos.called
+
+        # Confirm that the expected filtered list of todos was returned.
+        assert uncompleted_todos == [todo1]
+
+    def test_getting_uncompleted_todos_when_todos_is_none(self):
+        """Test getting uncomplete todos."""
+        # Configure mock to return None.
+        self.mock_get_todos.return_value = None
+
+        # Call the service, which will return an empty list.
+        uncompleted_todos = get_uncompleted_todos()
+
+        # Confirm that the mock was called.
+        assert self.mock_get_todos.called
+
+        # Confirm that an empty list was returned.
+        assert uncompleted_todos == []
